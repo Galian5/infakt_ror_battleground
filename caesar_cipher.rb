72c73ca3353f392a803777a6ac3c6de9989c3c52
Galian <galian5@wp.pl>
2018-05-30 14:19:00 +0200

    Initial commit

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,25 @@
-class CeasarCipher
+
+class CaesarCipher
+
+  def initialize(text, rot = 13)
+    @alphabet_down = Array('a'..'z').join
+    @alphabet_up = Array('A'..'Z').join
+    @text = text
+    @rot = rot
+    @cipher_down = @alphabet_down.chars.rotate(@rot).join
+    @cipher_up = @alphabet_up.chars.rotate(@rot).join
+  end
+
+  def perform
+    @text = @text = @text.split.each do |x|
+      if x.casecmp(x.downcase)
+        x.tr!(@alphabet_down, @cipher_down)
+      end
+      if x.casecmp(x.upcase)
+        x.tr!(@alphabet_up, @cipher_up)
+      end
+    end
+    @text.join(' ')
+  end
 
 end
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,20 @@
 class PassedOrFailed
+  def initialize(results, threshold)
+    @results = results.each { |key, value| results[key] = value.to_i}
+    @threshold = threshold.to_i
+    @passed = {}
+    @failed = {}
+  end
 
+
+  def perform
+    @results.each_pair do |student, score|
+      if score >= @threshold
+        @passed.store(student, score)
+      else
+        @failed.store(student, score)
+      end
+    end
+    {:passed => @passed, :failed => @failed}
+  end
 end
